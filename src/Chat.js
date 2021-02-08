effb5165f52df7b0abc26fed90612aed984ae89b
Samirul Alam <samirulalam372@gmail.com>
2021-02-09 00:12:05 +0600

    frontend completed

---
+++ src/Chat.js
@@ -0,0 +1,48 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import React from 'react';
+import "./Chat.css";
+import AttachFileOutlinedIcon from '@material-ui/icons/AttachFileOutlined';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+
+function Chat() {
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat__headerright">
+                    <IconButton>
+                        <SearchOutlinedIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileOutlinedIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message"><span className="chat__name">Samir</span>This is a message <span className="chat__timestamp">{new Date().toUTCString()}</span></p>
+                <p className="chat__message chat__reciever"><span className="chat__name">Samir</span>This is a message <span className="chat__timestamp">{new Date().toUTCString()}</span></p>
+                <p className="chat__message"><span className="chat__name">Samir</span>This is a message <span className="chat__timestamp">{new Date().toUTCString()}</span></p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input type="text" placeholder="Type a message"/>
+                    <button type="submit">Send a message</button>
+                </form>
+                <MicIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Chat
